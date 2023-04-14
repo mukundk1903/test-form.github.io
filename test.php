37a21d879d27e3d1ef9e35e2a261f706f0b1a0ed
mukundk1903 <77000172+mukundk1903@users.noreply.github.com>
2023-04-15 00:33:01 +0530

    Update test.php

--- test.php
+++ test.php
@@ -4,7 +4,7 @@ $name = $_POST['fname'];
 $lname = $_POST['lname'];
 ini_set('display_error', 1);
 error_reporting(E_ALL);
-$from = "noreply@legotrap.xyz";
+$from = "77000172+mukundk1903@users.noreply.github.com";
 $to = "mukundk712@gmail.com";
 $subject = "New Job Application";
 $message = "First Name = " . $name . "\r\n Last Name =" . $lname;
