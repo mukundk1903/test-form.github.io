a8db4f71e352dd1413e9a3152e4ab4a15fd1659a
mukundk1903 <77000172+mukundk1903@users.noreply.github.com>
2023-04-15 00:54:42 +0530

    Update test.php

--- test.php
+++ test.php
@@ -3,11 +3,6 @@
 $name = $_POST['Fname'];
 $lname = $_POST['Lname'];
 $email = $_POST['mail'];
-$phone = $_POST['tel'];
-$job = $_POST['Jobb'];
-$exp = $_POST['exp'];
-$link = $_POST['link'];
-$portf = $_POST['portf'];
 $to = "mukundk712@gmail.com";
 $subject = "Mail From website";
 $txt = "First Name = " . $name . "\r\n Last Name =" . $lname;
