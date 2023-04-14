6ab8088a60ab578024495defdc72598688267d04
mukundk1903 <77000172+mukundk1903@users.noreply.github.com>
2023-04-15 01:23:54 +0530

    Update test.php

--- test.php
+++ test.php
@@ -6,7 +6,7 @@ $email = $_POST['mail'];
 $to = "mukundk712@gmail.com";
 $subject = "Mail From website";
 $txt = "First Name = " . $name . "\r\n Last Name =" . $lname;
-$headers = "From: 77000172+mukundk1903@users.noreply.github.com" . "\r\n" .
+$headers = "From: https://mukundk1903.github.io/test-form.github.io/" . "\r\n" .
     "CC: somebodyelse@example.com";
 if ($email != NULL) {
     mail("mukundk712@gmail.com", "Mail from vmdesigngroup", $txt, $headers);
