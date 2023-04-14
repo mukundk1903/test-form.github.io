94acd272d17714a1280f7797307355094ac86f7c
mukundk1903 <77000172+mukundk1903@users.noreply.github.com>
2023-04-15 00:21:46 +0530

    Create test.php

---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+//get data from form  
+$name = $_POST['fname'];
+$lname = $_POST['lname'];
+ini_set('display_error', 1);
+error_reporting(E_ALL);
+$from = "noreply@legotrap.xyz";
+$to = "mukundk712@gmail.com";
+$subject = "New Job Application";
+$message = "First Name = " . $name . "\r\n Last Name =" . $lname;
+$headers = "From:" . $from;
+mail($to, $subject, $message, $headers);
+echo "<script type='text/javascript'>alert('Application Submitted Successfully. \nThank you')</script>";
