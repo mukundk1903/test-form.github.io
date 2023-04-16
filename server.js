6bc04fc60d53e68a0b8f935531860cfab8356d9e
mukundk1903 <77000172+mukundk1903@users.noreply.github.com>
2023-04-16 23:29:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -48,3 +48,53 @@ app.post('/submit-form', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+
+const app = express();
+
+// Set up middleware to parse form data
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Handle form submissions
+app.post('/send-email', (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const message = req.body.message;
+
+  // Create a new nodemailer transporter
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'mukundk712@gmail.com',
+      pass: 'cgpxpwlylgrsjyrm',
+    },
+  });
+
+  // Set up the email options
+  const mailOptions = {
+    from: 'mukundk712@gmail.com',
+    to: 'mukundk2410@gmail.com',
+    subject: 'New form submission',
+    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+  };
+
+  // Send the email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.send('Error: Could not send email');
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.send('Success: Email sent');
+    }
+  });
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
